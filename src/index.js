6922751c188b583cecab0cb268a1593d1e41415a
luizzhdz <luisangelhdz19@gmail.com>
2026-01-30 19:28:03 -0600

    puerto

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const { config } = require('./config');
 
 const app = createApp();
 
-app.listen(config.port, () => {
+app.listen(config.port, '127.0.0.1', () => {
   // eslint-disable-next-line no-console
   console.log(`[server] listening on :${config.port} (${config.nodeEnv})`);
 });
